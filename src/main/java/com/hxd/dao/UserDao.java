86fb3247259aa9210a2ecf4f94ffc3efedfb52ef
hee <heexudong@163.com>
2019-12-16 10:05:19 +0800

    super commit

--- src/main/java/com/hxd/dao/UserDao.java
+++ src/main/java/com/hxd/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.hxd.dao;
 
 import com.hxd.domain.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserDao {
 
     //查询所有
